25044167c6c13e0f5136078c499d29ef54935145
Ostroverhov, Vladislav <v.ostroverhov@a1qa.com>
2019-10-18 09:44:01 +0300

    sql queries fix name class Utils fix path in constant fix name columns fix simplify method

--- task5/src/main/java/Queries.java
+++ task5/src/main/java/Queries.java
@@ -1,28 +1,21 @@
 import java.io.IOException;
 import java.sql.SQLException;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
 
 public class Queries {
 
     public static void minWorkingTimeQuery() throws SQLException, IOException {
-        List<String> columns = Arrays.asList("PROJECT", "TEST", "MIN_WORKING_TIME");
-        Utils.outResultQuery(columns, "minWorkingTime.sql");
+        QueryUtils.outResultQuery("minWorkingTime.sql");
     }
 
     public static void amountUniqueTest() throws SQLException, IOException {
-        List<String> columns = Arrays.asList("PROJECT", "TEST_COUNT");
-        Utils.outResultQuery(columns, "amountUniqueTest.sql");
+        QueryUtils.outResultQuery("amountUniqueTest.sql");
     }
 
     public static void testsSinceDate() throws SQLException, IOException {
-        List<String> columns = Arrays.asList("PROJECT", "TEST", "DATE");
-        Utils.outResultQuery(columns, "testsSinceDate.sql");
+        QueryUtils.outResultQuery("testsSinceDate.sql");
     }
 
     public static void amountTestsFromBrowser() throws SQLException, IOException {
-        List<String> columns = Collections.singletonList("BROWSER");
-        Utils.outResultQuery(columns, "amountTestsFromBrowser.sql");
+        QueryUtils.outResultQuery("amountTestsFromBrowser.sql");
     }
 }
